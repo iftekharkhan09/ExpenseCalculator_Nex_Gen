65115146f04e3e8868e32fce0e9c16a90b211965
iftekharkhan09 <iftekharkhan629@gmail.com>
2017-06-02 21:39:26 +0530

    Commiting the Changes.

--- src/main/java/com/expensecalculator/service/impl/StaffServiceImpl.java
+++ src/main/java/com/expensecalculator/service/impl/StaffServiceImpl.java
@@ -8,9 +8,14 @@ import com.expensecalculator.ui.beans.LoginBean;
 public class StaffServiceImpl implements StaffService {
 	@Override
 	public Staff authenticateStaff(LoginBean loginBean) {
-		Staff staff = new StaffDaoImpl().findUnique(loginBean.getEmailId());
+		Staff staff = new StaffDaoImpl().findUnique("we");
 		if (staff != null)
 			return staff;
 		return null;
+	}
+	public static void main(String[] args) {
+		Staff staff=new StaffServiceImpl().authenticateStaff(null);
+		System.out.println(staff);
+		System.out.println(staff.getId());
 	}
 }
