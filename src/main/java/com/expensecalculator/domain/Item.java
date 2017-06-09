21b5b32e8988a94310ff3ffe8bba2bf436e54cc4
iftekharkhan09 <iftekharkhan629@gmail.com>
2017-06-09 23:54:47 +0530

    Commiting the Item Entity.

--- src/main/java/com/expensecalculator/domain/Item.java
+++ src/main/java/com/expensecalculator/domain/Item.java
@@ -1,39 +1,57 @@
 package com.expensecalculator.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ITEM")
 public class Item {
-	Integer item_id;
-	String Description;
-	Integer unit_price_id;
-	int Category_id;
-	int Measurement_id;
-	public Integer getItem_id() {
-		return item_id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String Description;
+	@OneToOne
+	@JoinColumn(name = "unit_id")
+	private Unit unit;
+	@OneToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	public int getId() {
+		return id;
 	}
-	public void setItem_id(Integer item_id) {
-		this.item_id = item_id;
+
+	public void setId(int id) {
+		this.id = id;
 	}
+
 	public String getDescription() {
 		return Description;
 	}
+
 	public void setDescription(String description) {
 		Description = description;
 	}
-	public Integer getUnit_price_id() {
-		return unit_price_id;
+
+	public Unit getUnit() {
+		return unit;
 	}
-	public void setUnit_price_id(Integer unit_price_id) {
-		this.unit_price_id = unit_price_id;
+
+	public void setUnit(Unit unit) {
+		this.unit = unit;
 	}
-	public int getCategory_id() {
-		return Category_id;
+
+	public Category getCategory() {
+		return category;
 	}
-	public void setCategory_id(int category_id) {
-		Category_id = category_id;
-	}
-	public int getMeasurement_id() {
-		return Measurement_id;
-	}
-	public void setMeasurement_id(int measurement_id) {
-		Measurement_id = measurement_id;
+
+	public void setCategory(Category category) {
+		this.category = category;
 	}
 }
