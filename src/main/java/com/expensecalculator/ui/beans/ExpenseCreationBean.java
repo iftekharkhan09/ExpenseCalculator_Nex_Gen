d5d2fa79aa32915bbed89893edbf90b4d1b798c9
Iftekhar.a <Iftekhar.a@BSL-BNG-L155.betsol.com>
2017-08-02 01:00:32 +0530

    Added for Expense UI.

---
+++ src/main/java/com/expensecalculator/ui/beans/ExpenseCreationBean.java
@@ -0,0 +1,12 @@
+package com.expensecalculator.ui.beans;
+
+import com.expensecalculator.domain.Category;
+import com.expensecalculator.domain.Unit;
+import com.expensecalculator.domain.User;
+
+public class ExpenseCreationBean {
+	private User user;
+	private double amountPaid;
+	private Category category;
+	private Unit unit;
+}
